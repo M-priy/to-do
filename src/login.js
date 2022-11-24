e7d586a1348bb442affc9b5c30a527006034774a
M-priy <727721eucd064@skcet.ac.in>
2022-11-24 11:40:13 +0530

    mad

---
+++ src/login.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './login.css'
+function login() {
+  return (
+    <div>
+        <div id="login-form-wrap">
+  <h2>Login</h2>
+  <form id="login-form">
+    <p>
+    <input type="text" id="username" name="username" placeholder="Username" required><i class="validation"><span></span><span></span></i></input>
+    </p>
+    <p>
+    <input type="email" id="email" name="email" placeholder="Email Address" required><i class="validation"><span></span><span></span></i></input>
+    </p>
+    <p>
+    <input type="submit" id="login" value="Login"></input>
+    </p>
+  </form>
+  <div id="create-account-wrap">
+    <p>Not a member? <a href="#">Create Account</a></p>
+  </div>
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default login
